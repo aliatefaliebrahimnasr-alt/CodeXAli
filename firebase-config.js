9b1d15e7e1b331c0f686608655a516011e780a1c
AN <aliatefaliebrahimnasr@gmail.com>
2026-09-03 00:41:54 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.PRIMECART_FIREBASE = {
+  apiKey: "AIzaSyBVc658KheBNPIagzpXMbnmGb36EXwU3iA",
+  authDomain: "alinasr-890e4.firebaseapp.com",
+  databaseURL: "https://alinasr-890e4-default-rtdb.firebaseio.com",
+  projectId: "alinasr-890e4",
+  storageBucket: "alinasr-890e4.firebasestorage.app",
+  messagingSenderId: "856869991035",
+  appId: "1:856869991035:web:f32eca9d9eaeffe12ba8de"
+};
